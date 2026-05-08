1a06e504ab6c5807aeea0ad8a6df862489902aaf
faustinalardi45-netizen <faustinalardi45@gmail.com>
2026-05-08 08:32:14 +0000

    Update fal_6307824.f90

--- MET253_Assignment_repo/fal_6307824.f90
+++ MET253_Assignment_repo/fal_6307824.f90
@@ -12,14 +12,19 @@ d(7)=55
 d(8)=88
 d(9)=61
 d(10)=47
-if(d>=80) then print*,'Distinction' 
-else if (d>=60)
- print*,'credit'
-else if (d>=40) then 
-print*,'pass'
-else if (d>=39) then print*,'fail'
-end if
+!Added a DO loop before the IF statement so each score can be checked one by one
 do i =1,10
-read*, d(i)
+if(d(i)>=80) then
+print*, d(i), 'Distinction'
+!Added THEN after else if condition
+else if (d(i)>=60) then
+ print*, d(i), 'credit'
+else if (d(i)>=40) then 
+print*, d(i), 'pass'
+!changed else if d>=40 to else because all values below 40 is fail
+else 
+print*, d(i), 'fail'
+end if
+!removed read statement because the valyes were assigned earlier
 end do
 end program results
